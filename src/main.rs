7360c9770c74b906d4d8509cd1d2304495c6d69e
Gedalia Kott <gedkott@gmail.com>
2021-04-07 00:09:42 -0700

    print response body for CLI argument of URL
    updated data structures to use indexes instead of
    str slices because either self-referential structs
    are not permitted or I was doing it wrong

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
+use std::collections::HashMap;
+use std::env;
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::collections::HashMap;
 
 #[derive(Debug)]
 struct Url<'surl> {
     scheme: &'surl str,
     host: &'surl str,
-    path: &'surl str
+    path: &'surl str,
 }
 
 #[derive(Debug)]
 struct StatusLine<'resp> {
     version: &'resp str,
     status: &'resp str,
-    explanation: &'resp str
-}
-
-impl StatusLine<'_> {
-    fn new() -> Self {
-        StatusLine {
-            version: "",
-            status: "",
-            explanation: ""
-        }
-    }
+    explanation: &'resp str,
 }
 
 type Headers<'resp> = HashMap<&'resp str, &'resp str>;
@@ -31,59 +22,62 @@ type Headers<'resp> = HashMap<&'resp str, &'resp str>;
 #[derive(Debug)]
 struct Response {
     response_buffer: String,
+    status_line_end_index: usize,
+    headers_end_index: usize,
 }
 
 impl Response {
-    fn new(response_buffer: String) -> Self {    
-        println!("{:?}", response_buffer);
-    
-        // let status_line = get_status_line(&response_buffer)
-    
-        // let mut parts = response_buffer[index..].split("\r\n\r\n");
-        // let headers_string = parts.next().unwrap();
-        // let headers = get_headers(headers_string);
-        // let body = parts.next().unwrap();
-    
+    fn new(response_buffer: String) -> Self {
+        let status_line_end_index = response_buffer.find("\r\n").unwrap();
 
-        // let response = 
-        // println!("{:?}", response);
-        // // println!("{:?}", body);
-    
-        // response
-        let (status_line, body): (StatusLine<'_>, usize) = {
-            let mut parts = response_buffer.match_indices("\r\n\r\n");
-            let (status_line_and_headers_index, status_line_and_headers) = parts.next().unwrap();
-            let (body_index, _) = parts.next().unwrap();
-    
-            let status_line = Response::get_status_line(status_line_and_headers).unwrap();
-            (status_line, body_index)
-        };
+        let headers_end_index = response_buffer.find("\r\n\r\n").unwrap();
 
         Response {
             response_buffer: response_buffer,
+            status_line_end_index,
+            headers_end_index,
         }
     }
 
-    fn get_status_line(response_buffer: &str) -> Option<StatusLine<'_>> {
-        // get status line
-        for i in 0..response_buffer.len() {
-            if response_buffer.chars().nth(i) == Some('\r') && response_buffer.chars().nth(i + 1) == Some('\n') {
-                let status_line_string = &response_buffer[0..i + 2];
-                let mut parts = status_line_string.split(" ");
-                let count = status_line_string.split(" ").count();
-                if count != 3 {
-                    return None
-                } else {
-                    return Some(StatusLine {
-                        version: parts.next().unwrap(),
-                        status: parts.next().unwrap(),
-                        explanation: parts.next().unwrap()
-                    })
+    fn get_status_line(&self) -> Option<StatusLine> {
+        let mut parts = self.response_buffer[0..self.status_line_end_index].split(" ");
+
+        let version = parts.next().unwrap();
+        let status = parts.next().unwrap();
+        let explanation = parts.next().unwrap();
+
+        Some(StatusLine {
+            version,
+            status: status,
+            explanation,
+        })
+    }
+
+    fn get_headers(&self) -> Headers {
+        let kvs = self.response_buffer[self.status_line_end_index + 2..self.headers_end_index]
+            .split("\r\n")
+            .filter(|line| !line.is_empty())
+            .map(|header_string| {
+                let mut parts = header_string.split(": ");
+                let key = parts.next();
+                let value = parts.next();
+                (key, value)
+            })
+            .filter_map(|kv_pair| {
+                let (key, value) = kv_pair;
+                match (key, value) {
+                    (Some(k), Some(v)) => Some((k, v)),
+                    _ => None,
                 }
-            }
+            });
+        HashMap::from(kvs.collect())
+    }
+
+    fn get_body(&self) -> Body {
+        Body {
+            body_buffer: &self.response_buffer
+                [self.headers_end_index + 4..self.response_buffer.len() - 1],
         }
-    
-        return None
     }
 }
 
@@ -102,71 +96,61 @@ fn get_host(surl: &str) -> &str {
     &surl[0..curr_index]
 }
 
-fn get_headers(response_buffer: &str) -> Headers {
-    let kvs = response_buffer
-        .split("\r\n")
-        .filter(|line| !line.is_empty())
-        .map(|header_string| {
-            let mut parts = header_string.split(": ");
-            let key = parts.next();
-            let value = parts.next();
-            (key, value)
-        })
-        .filter_map(|kv_pair| {
-            let (key, value) = kv_pair;
-            match (key, value) {
-                (Some(k), Some(v)) => {
-                    Some((k,v))
-                }
-                _ => None
-            }
-        });
-    HashMap::from(kvs.collect())
-}
-
 impl<'surl> Url<'surl> {
     fn new(surl: &'surl str) -> Option<Self> {
         let scheme = &surl[0..7];
         let host = get_host(&surl[7..]);
         let path = &surl[7 + host.len()..];
         if scheme == "http://" {
-            Some(Url {
-                scheme,
-                host,
-                path
-            })
+            Some(Url { scheme, host, path })
         } else {
             None
         }
     }
 }
 
-type Body<'resp> = &'resp str;
+#[derive(Debug)]
+struct Body<'resp> {
+    body_buffer: &'resp str,
+}
 
 fn request<'surl, 'resp>(url: &'surl str) -> Response {
     let my_url = Url::new(url).unwrap();
-    println!("{:?}", my_url);
+    let http_string =
+        String::from("GET /index.html HTTP/1.0\r\n") + &format!("Host: {}\r\n\r\n", my_url.host);
 
     let mut stream = TcpStream::connect((my_url.host, 80)).unwrap();
-    println!("{:?}", stream);
-
-
-    let http_string = String::from("GET /index.html HTTP/1.0\r\n") + 
-                      "Host: example.org\r\n\r\n";
-    
-    let request_bytes = stream.write(http_string.as_bytes()).unwrap();
-    println!("{:?}", request_bytes);
+    let _ = stream.write(http_string.as_bytes()).unwrap();
 
     let mut response_buffer = String::new();
     let _ = stream.read_to_string(&mut response_buffer).unwrap();
-    let response = Response::new(response_buffer);
-    
-    response
+    Response::new(response_buffer)
+}
+
+fn print_body(body: &Body) {
+    let mut in_angle = false;
+    body.body_buffer.chars().for_each(|c| {
+        if c == '<' {
+            in_angle = true;
+        } else if c == '>' {
+            in_angle = false;
+        } else {
+            if !in_angle {
+                print!("{}", c)
+            }
+        }
+    });
 }
 
 fn main() {
-    let url = "http://example.org/index.html"; 
-    
-    request(url);
-    
+    let args = env::args().collect::<Vec<String>>();
+    let url = if args.len() == 2 {
+        &args[1]
+    } else {
+        "http://example.org/index.html"
+    };
+
+    let response = request(url);
+
+    print_body(&response.get_body());
 }
